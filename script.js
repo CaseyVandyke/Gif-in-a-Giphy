150b6ec4dda090d3134542f6dd41d269fc15c3e8
Cryosith <caseyvandyke@ymail.com>
2018-06-23 12:00:08 -0600

    api call

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-$()
+function tubeSearch(search) {
+
+    const key = 'AIzaSyDCnL8sLhq05QgnhiwqvMFCDbKsAhYfmbA';
+    const URL = 'https://www.googleapis.com/youtube/v3/search';
+
+    const params = {
+        part: 'snippet',
+        key: key,
+        q: search
+    }
+
+   
+        $.getJSON(URL, params, function (data) {
+            $('.results').html('');
+            for(let i = 0; i < data.items.length; i++) {
+               $('.results').append(`<h3>${data.items[i].snippet.title}</h3>`);
+                 $('.results').append(`<img src="${data.items[i].snippet.thumbnails.medium.url}"/>`);
+                
+            }
+    })
+
+}
+
+$('.js-catcher').on('submit', function (event) {
+    event.preventDefault();
+    let searchHolder = $('#usr').val();
+    tubeSearch(searchHolder);
+});
