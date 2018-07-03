19983232e6f1e77499b6409010ebe5f532384095
Cryosith <caseyvandyke@ymail.com>
2018-07-02 18:06:13 -0600

    responsive

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function getDataFromGiphy(search) {
-    
+
     const URL = 'http://api.giphy.com/v1/gifs/search';
     // set up the query
     const query = {
         api_key: 'RVU0bZkFNjUyjUwwV8wHHrO37B0V01jy',
         q: search,
-        limit: 150,
+        limit: 9,
     }
 
     $.getJSON(URL, query, function (gif) {
         $('.results').html('');
-        for (let i = 0; i <gif.data.length; i++) {
+        for (let i = 0; i < gif.data.length; i++) {
             $('.results').append(`<img class="gif-box" src="${gif.data[i].images.fixed_height_small.url}" alt="${gif.data[i].images}">`);
-            
         }
+
     })
+
 }
 
 $('.js-catcher').on('submit', function (event) {
