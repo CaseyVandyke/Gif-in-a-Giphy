f86224174864e2d28e5d21e23ed71b4f86a0df39
Cryosith <caseyvandyke@ymail.com>
2018-07-05 23:20:55 -0600

    dropdown

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getDataFromGiphy(search) {
     const query = {
         api_key: 'RVU0bZkFNjUyjUwwV8wHHrO37B0V01jy',
         q: search,
-        limit: 50,
+        limit: 25,
     }
 
     $.getJSON(URL, query, function (gif) {
@@ -18,8 +18,39 @@ function getDataFromGiphy(search) {
 
 }
 
+
+
+function getDataForSticker(search) {
+
+    const URL = 'http://api.giphy.com/v1/stickers/search';
+    // set up the query
+    const query = {
+        api_key: '7Q2Zlr9K4UiisLdt8XuwfGiZXEHuxr7n',
+        q: search,
+        limit: 24,
+    }
+
+    $.getJSON(URL, query, function (sticker) {
+        $('.sticker-results').html('');
+        for (let i = 0; i < sticker.data.length; i++) {
+            $('.sticker-results').append(`<img class="gif-box" src="${sticker.data[i].images.fixed_height_small.url}" alt="${sticker.data[i].images}">`);
+        }
+
+    })
+}
+
+$("#gif-select").change(function() {
+    let selectedGif = $("#gif-select option:selected").val();
+
+
 $('.js-catcher').on('submit', function (event) {
     event.preventDefault();
     let userInfo = $('#usr').val();
-    getDataFromGiphy(userInfo);
+    if (selectedGif === "gif") {
+        getDataFromGiphy(userInfo);
+    } else if(selectedGif === "sticker"){
+        getDataForSticker(userInfo);
+    }
+
 })
+});
