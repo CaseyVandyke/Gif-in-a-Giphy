3b2ed82046f5d1e1d63d672a5c2877da84bfd531
Cryosith <caseyvandyke@ymail.com>
2018-07-11 23:13:59 -0600

    trending

--- script.js
+++ script.js
@@ -1,87 +1,71 @@
-
 // Retrieves user input for gifs 
 function getDataFromGiphy(search, numHolder) {
 
-    const URL = 'http://api.giphy.com/v1/gifs/search';
+    const URL = 'https://api.giphy.com/v1/gifs/search';
     // set up the query
     const query = {
         api_key: 'RVU0bZkFNjUyjUwwV8wHHrO37B0V01jy',
         q: search,
         limit: numHolder
     }
-
     $.getJSON(URL, query, function (gif) {
         clearResults();
         for (let i = 0; i < gif.data.length; i++) {
             $('.results').append(`<img class="gif-box" src="${gif.data[i].images.fixed_height_small.url}" alt="${gif.data[i].images}" title="${gif.data[i].title}">`);
         }
-
     })
-
 }
 
 // Retrieves user input for stickers 
 
 function getDataForSticker(search, numHolder) {
 
-    const URL = 'http://api.giphy.com/v1/stickers/search';
+    const URL = 'https://api.giphy.com/v1/stickers/search';
     // set up the query
     const query = {
         api_key: 'RVU0bZkFNjUyjUwwV8wHHrO37B0V01jy',
         q: search,
         limit: numHolder
     }
-
     $.getJSON(URL, query, function (sticker) {
         clearResults();
         for (let i = 0; i < sticker.data.length; i++) {
             $('.results').append(`<img class="gif-box" src="${sticker.data[i].images.fixed_height_small.url}" alt="${sticker.data[i].images}">`);
         }
-
     })
 }
 
 // Retrieves user input for random gifs 
 
-function getDataForRandomGif(search) {
+function getDataForRandomGif(numHolder) {
 
-    const URL = 'http://api.giphy.com/v1/gifs/random';
+    const URL = 'https://api.giphy.com/v1/gifs/random';
     // set up the query
     const query = {
         api_key: 'RVU0bZkFNjUyjUwwV8wHHrO37B0V01jy',
-        tag: search
     }
-
-    $.getJSON(URL, query, function (random) {
-        clearResults()
-        var newArr = $.makeArray(random.data);
-        for (let i = 0; i < newArr.length; i++) {
-            $('.results').append(`<img class="gif-box" src="${newArr[i].images.fixed_height_small.url}" alt="${newArr[i].images}" title="${newArr[i].title}">`);
-        }
-    })
-
+    clearResults()
+    for (let i = 0; i < numHolder; i++) {
+        $.getJSON(URL, query, function (random) {
+            $('.results').append(`<img class="gif-box" src="${random.data.images.fixed_height_small.url}" alt="${random.data.images}" title="${random.data.title}">`);
+        })
+    }
 }
 
-// Retrieves user input for random stickers 
+function getDataForTrendingGif(numHolder) {
 
-function getDataForRandomSticker(search) {
-
-    const URL = 'http://api.giphy.com/v1/stickers/random';
+    const URL = 'https://api.giphy.com/v1/gifs/trending';
     // set up the query
     const query = {
         api_key: 'RVU0bZkFNjUyjUwwV8wHHrO37B0V01jy',
-        tag: search
-        
+        limit: numHolder
     }
-
-    $.getJSON(URL, query, function (random) {
-        clearResults()
-        var newArr = $.makeArray(random.data);
-        for (let i = 0; i < newArr.length; i++) {
-            $('.results').append(`<img class="gif-box" src="${newArr[i].images.fixed_height_small.url}" alt="${newArr[i].images}">`);
-        }
-    })
-
+    clearResults()
+    for (let i = 0; i < numHolder; i++) {
+        $.getJSON(URL, query, function (trending) {
+            $('.results').append(`<img class="gif-box" src="${trending.data[i].images.fixed_height_small.url}" alt="${trending.data[i].images}" title="${trending.data[i].title}">`);
+        })
+    }
 }
 
 // Clears user search 
@@ -94,24 +78,33 @@ function clearResults() {
 // Button that retrieves different search results depending on user selection 
 
 $('.js-catcher').on('submit', function (event) {
-    event.preventDefault();
-    let numberHolder = $('.user-number').val();
-    let selectedGif = $("#gif-select option:selected").val();
-    let userInfo = $('#usr').val();
-    
-    if (selectedGif === "gif") {
-        clearResults();
-        getDataFromGiphy(userInfo, numberHolder);
-        
-    } else if (selectedGif === "sticker") {
-        clearResults();
-        getDataForSticker(userInfo, numberHolder);
-    } else if (selectedGif === "random") {
-        clearResults();
-        getDataForRandomGif(userInfo, numberHolder);
-    } else if (selectedGif === "random-sticker") {
-        clearResults();
-        getDataForRandomSticker(userInfo, numberHolder);
-    }
+            event.preventDefault();
+            let numberHolder = $('.user-number').val();
+            let selectedGif = $("#gif-select option:selected").val();
+            let userInfo = $('#usr').val();
+            if (userInfo.trim() === $('#usr').val("")) {
+                alert('No Homo');
+            } else {
+                if (selectedGif === "gif") {
+                    clearResults();
+                    getDataFromGiphy(userInfo, numberHolder);
+                } else if (selectedGif === "sticker") {
+                    clearResults();
+                    getDataForSticker(userInfo, numberHolder);
+                } else if (selectedGif === "random") {
+                    clearResults();
+                    getDataForRandomGif(numberHolder);
+                } else if (selectedGif === "trending") {
+                    clearResults();
+                    getDataForTrendingGif(numberHolder);
+                }
+            }
+        });
 
-});
+        $('#gif-select').on('change', function (event) {
+            if ($(this).val() == 'random' || ($(this).val() == 'trending')) {
+                $('#usr').hide();
+            } else {
+                $('#usr').show();
+            }
+        });
